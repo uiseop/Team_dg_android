a4fd360091a26e7876e0c592017c11a8731ca4b9
Park YS <pinoa1228@naver.com>
2021-02-14 22:44:18 +0900

    Merge branch 'master' of https://github.com/uiseop/0116
     Conflicts:
    	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/course/capstone/activities/TaxActivity.java
+++ app/src/main/java/com/course/capstone/activities/TaxActivity.java
@@ -35,7 +35,7 @@ public class TaxActivity extends AppCompatActivity {
         TManager = new LinearLayoutManager(this);
         taxrecyclerview.setLayoutManager(TManager);
         ArrayList<EducationCard> Tax = new ArrayList<>();
-        Tax.add(new EducationCard(R.drawable.tax_1,"비과세","국세청 등이 세금을 부과하는 과세권이  처음부터 없는 것으로, 세금 자체가 발생하지 않으므로 신고의 의무도 없다.감면vs비과세 감면: 감면요건을 충족한 뒤 신고하고 감면 신청을 해야하는 등 신고의무와 납세협력의무가 있다. 비과세: 당초 세금 자체가 발생하지 않으므로 세금을 신고,신청 납부할 필요가 없다."));
+        Tax.add(new EducationCard(R.drawable.tax_1,"비과세","국세청 등이 세금을 부과하는 과세권이  처음부터 없는 것으로, 세금 자체가 발생하지 않으므로 신고의 의무도 없다.\n" +"Q.감면vs비과세\n" +"감면: 감면요건을 충족한 뒤 신고하고 감면 신청을 해야하는 등 신고의무와 납세협력의무가 있다.\n" +"비과세: 당초 세금 자체가 발생하지 않으므로 세금을 신고,신청 납부할 필요가 없다."));
         Tax.add(new EducationCard(R.drawable.tax_2,"비과세소득","비과세 소득은 세법에서 특별히 과세하지 않는 소득으로 규정한 소득이다.근로자 본인의 학자금, 실업급여, 출산&보육수당, 숙직료, 자가 운전 보조금등이 있다."));
         Tax.add(new EducationCard(R.drawable.tax_3,"세후","세금을 공제 받은 후 실수령 금액을 말한다."));
         Tax.add(new EducationCard(R.drawable.tax_4,"세전","세금을 공제 받기 전의 금액을 말한다."));
